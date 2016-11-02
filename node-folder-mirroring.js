881cf494c14d7ad849d40031b39b506ada685263
Francesco Longo <flongo@unime.it>
2016-11-02 17:00:09 +0100

    First version of the tool

---
+++ node-folder-mirroring.js
@@ -0,0 +1,350 @@
+var fuse = require('fuse-bindings');
+var fs = require('fs');
+var mknod = require('mknod');
+var pt = require('path');
+var statvfs = require('statvfs');
+
+if (process.argv.length != 4){
+    console.log("Usage: node mirror.js <original_folder> <mirror_folder>");
+    process.exit(-1);
+}
+
+var original_folder = process.argv[2];
+var mirror_folder = process.argv[3];
+
+
+if (!pt.isAbsolute(original_folder) || !pt.isAbsolute(mirror_folder)){
+    console.log("Please use absolute paths!");
+    process.exit(-1);
+}
+
+console.log("Mounting folder " + original_folder + " in folder " + mirror_folder);
+
+    var getattr_function = function(path, cb){
+        console.log('getattr(%s)',path);
+        fs.lstat(pt.join(original_folder, path), function(err, stats){
+            if(err){
+                //console.log('error: ', err);                
+                cb(fuse[err.code]);
+            }
+            else{
+                cb(null,stats);
+            }
+        });
+    }
+    
+    var access_function = function(path, mode, cb){
+        console.log('access(%s)', path);
+        fs.access(pt.join(original_folder, path), mode, function(err){
+            if(err){
+                //console.log('error: ', err);
+                cb(fuse[err.code]);
+            }
+            else{
+                cb(0);
+            }
+        });
+    }
+    
+    var readlink_function = function(path, cb){
+        console.log('readlink(%s)', path);
+        fs.readlink(pt.join(original_folder, path), function(err, linkString){
+            if(err){
+                //console.log('error: ', err);
+                cb(fuse[err.code]);
+            }
+            else{
+                cb(null, linkString);
+            }
+        });        
+    }
+    
+    var readdir_function = function(path, cb){
+        console.log('readdir(%s)', path);
+        fs.readdir(pt.join(original_folder, path), function(err, files){
+            if(err){
+                //console.log('error: ', err);
+                cb(fuse[err.code]);
+            }
+            else{
+                console.log('files: ', files);
+                cb(null,files);
+            }
+        });                
+    }
+    
+    var mknod_function = function(path, mode, dev, cb){
+        console.log('mknod(%s)', path);
+        mknod(pt.join(original_folder, path), mode, dev, function (err) {
+            if(err){
+                //console.log('error: ', err);
+                cb(fuse[err.code]);
+            }
+            else{
+                cb(0);
+            }            
+        });
+    }
+    
+    var mkdir_function = function(path, mode, cb){
+        console.log('mkdir(%s)', path);
+        fs.mkdir(pt.join(original_folder, path), mode, function (err) {
+            if(err){
+                //console.log('error: ', err);
+                cb(fuse[err.code]);
+            }
+            else{
+                cb(0);
+            } 
+        });        
+    }
+    
+    var unlink_function = function(path, cb){
+        console.log('unlink(%s)', path);
+        fs.unlink(pt.join(original_folder, path), function (err) {
+            if(err){
+                //console.log('error: ', err);
+                cb(fuse[err.code]);
+            }
+            else{
+                cb(0);
+            } 
+        });        
+    }
+    
+    var rmdir_function = function(path, cb){
+        console.log('rmdir(%s)', path);
+        fs.rmdir(pt.join(original_folder, path), function (err) {
+            if(err){
+                //console.log('error: ', err);
+                cb(fuse[err.code]);
+            }
+            else{
+                cb(0);
+            } 
+        });                
+    }
+    
+    var symlink_function = function(src, dest, cb){
+        console.log('symlink(%s,%s)', src, dest);
+        fs.symlink(pt.join(original_folder, src), pt.join(original_folder, dest), function (err) {
+            if(err){
+                //console.log('error: ', err);
+                cb(fuse[err.code]);
+            }
+            else{
+                cb(0);
+            } 
+        });        
+    }
+    
+    var rename_function = function(src, dest, cb){
+        console.log('rename(%s,%s)', src, dest);
+        fs.rename(pt.join(original_folder, src), pt.join(original_folder, dest), function (err) {
+            if(err){
+                //console.log('error: ', err);
+                cb(fuse[err.code]);
+            }
+            else{
+                cb(0);
+            } 
+        });                
+    }
+    
+    var link_function = function(src, dest, cb){
+        console.log('link(%s,%s)', src, dest);
+        fs.link(pt.join(original_folder, src), pt.join(original_folder, dest), function (err) {
+            if(err){
+                //console.log('error: ', err);
+                cb(fuse[err.code]);
+            }
+            else{
+                cb(0);
+            } 
+        });                        
+    }
+    
+    var chmod_function = function(path, mode, cb){
+        console.log('chmod(%s)', path);
+        fs.chmod(pt.join(original_folder, path), mode, function (err) {
+            if(err){
+                //console.log('error: ', err);
+                cb(fuse[err.code]);
+            }
+            else{
+                cb(0);
+            } 
+        });                                
+    }
+    
+    var chown_function = function(path, uid, gid, cb){
+        console.log('chown(%s)', path);
+        fs.chown(pt.join(original_folder, path), uid, gid, function (err) {
+            if(err){
+                //console.log('error: ', err);
+                cb(fuse[err.code]);
+            }
+            else{
+                cb(0);
+            } 
+        });        
+    }
+    
+    var truncate_function = function(path, size, cb){
+        console.log('truncate(%s)', path);
+        fs.truncate(pt.join(original_folder, path), size, function (err) {
+            if(err){
+                //console.log('error: ', err);
+                cb(fuse[err.code]);
+            }
+            else{
+                cb(0);
+            } 
+        });        
+    }
+    
+    var utimens_function = function(path, atime, mtime, cb){
+        console.log('utimens(%s)', path);
+        fs.utimes(pt.join(original_folder, path), atime, mtime, function (err) {
+            if(err){
+                //console.log('error: ', err);
+                cb(fuse[err.code]);
+            }
+            else{
+                cb(0);
+            } 
+        });        
+    }
+    
+    var open_function = function(path, flags, cb){
+        console.log('open(%s)', path);
+        fs.open(pt.join(original_folder, path), flags, function (err, fd) {
+            if(err){
+                //console.log('error: ', err);
+                cb(fuse[err.code]);
+            }
+            else{
+                cb(null, fd);
+            } 
+        });        
+    }
+    
+    var read_function = function(path, fd, buffer, length, position, cb){
+        console.log('read(%s)', path);
+        fs.open(pt.join(original_folder, path), 'r', function (err, int_fd) {
+            if(err){
+                //console.log('error: ', err);
+                cb(fuse[err.code]);
+            }
+            else{
+                fs.read(int_fd, buffer, 0, length, position, function(err, bytesRead, int_buffer){
+                    if(err){
+                        //console.log('error: ', err);
+                        cb(fuse[err.code]);
+                    }
+                    else{
+                        buffer.copy(int_buffer);
+                        fs.close(int_fd, function(err){
+                            if(err){
+                                //console.log('error:', err);
+                                cb(fuse[err.code]);
+                            }
+                            else{
+                                cb(bytesRead);
+                            }
+                        });
+                    }
+                });
+            } 
+        });        
+    }
+    
+    var write_function = function(path, fd, buffer, length, position, cb){
+        console.log('write(%s)', path);
+        fs.open(pt.join(original_folder, path), 'r+', function (err, int_fd) {
+            if(err){
+                //console.log('error: ', err);
+                cb(fuse[err.code]);
+            }
+            else{
+                fs.write(int_fd, buffer, 0, length, position, function(err, written, int_buffer){
+                    if(err){
+                        //console.log('error: ', err);
+                        cb(fuse[err.code]);
+                    }
+                    else{
+                        fs.close(int_fd, function(err){
+                            if(err){
+                                //console.log('error:', err);
+                                cb(fuse[err.code]);
+                            }
+                            else{
+                                cb(written);
+                            }
+                        });
+                    }
+                });
+            } 
+        });        
+
+    }
+    
+    var getxattr_function = function(path, name, buffer, length, offset, cb){
+        console.log('getxattr_function(%s)', path);
+        fs.lstat(pt.join(original_folder, path), function(err, stats){
+            if(err){
+                console.log('error: ', err);
+                cb(fuse[err.code]);
+            }
+            else{
+                console.log('stats: ', stats);
+                cb(null,stats);
+            }
+        });
+        
+    }
+    
+    var statfs_function = function(path, cb){
+        console.log('statvfs(%s)', path);
+        statvfs(pt.join(original_folder, path), function(err, stats){
+            if(err){
+                //console.log('error: ', err);
+                cb(fuse[err.code]);
+            }
+            else{
+                cb(null,stats);
+            }
+        });
+    }
+    
+fuse.mount(mirror_folder, {
+    getattr: getattr_function,
+    access: access_function,
+    readlink: readlink_function,
+    readdir: readdir_function,
+    mknod: mknod_function, //(not available in fs, using mknod module instead)
+    mkdir: mkdir_function,
+    unlink: unlink_function,
+    rmdir: rmdir_function,
+    symlink: symlink_function,
+    rename: rename_function,
+    link: link_function,
+    chmod: chmod_function,
+    chown: chown_function,
+    truncate: truncate_function,
+    utimens: utimens_function,
+    open: open_function,
+    read: read_function,
+    write: write_function,
+    statfs: statfs_function, //(not available in fs, using statvfs module instead)
+    //setxattr: setxattr_function (not available in fs or other implementations)
+    getxattr: getxattr_function //(not available in fs or other implementations, using lstat instead)
+    //listxattr: (not available in fuse-bindings)
+    //removexattr: (not available in fuse-bindings)
+});
+
+process.on('SIGINT', function () {
+  fuse.unmount(mirror_folder, function () {
+    process.exit()
+  })
+})
